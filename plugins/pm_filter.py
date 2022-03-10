42a109d56bad815e5610b82709f591c0dd2bdb77
Akshayss47 <99110076+Akshayss47@users.noreply.github.com>
2022-03-10 09:45:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -681,13 +681,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/movierequestgroupCU'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/movierequestgroupCU')
+            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/allmovierequestuc'),
+            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/allmovierequestuc')
             ],[      
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://t.me/movierequestgroupCU')
+            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://t.me/allmovierequestuc')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
@@ -824,7 +824,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton('♥️ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 ♥️', url='https://t.me/movierequestgroupCU')
+            InlineKeyboardButton('♥️ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 ♥️', url='https://t.me/allmovierequestuc')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
